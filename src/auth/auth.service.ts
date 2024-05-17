2dc8bb086b1223ccc420fcf68e8280215d1fc13e
vrungel <arsenyan.m.i20@gmail.com>
2024-05-17 21:21:22 +0300

    user validation during registration and authorization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,34 +20,34 @@ export class AuthService {
   async signUp(signUpDto: SignUpDto): Promise<{ message: string, statusCode: number, token?: string }> {
     const { name, email, password } = signUpDto;
     const existingUser = await this.userModel.findOne({ email });
-  
+
     if (existingUser) {
       if (existingUser.isVerified) {
         return { message: 'Email is already registered and verified.', statusCode: 400 };
       }
-  
-      // Пользователь существует, но не верифицирован
+
+      // Пользователь существует, но не верифицирован      
       // Обновляем все поля, кроме email
       const verificationCode = generateVerificationCode();
       const hashedPassword = await bcrypt.hash(password, 10);
-  
+
       existingUser.name = name;
       existingUser.password = hashedPassword;
       existingUser.verificationCode = verificationCode;
       existingUser.isVerified = false;
       
       await existingUser.save();
-  
       // Можно отправить новое письмо с кодом подтверждения
       await this.sendVerificationEmail(existingUser.email, verificationCode);
-  
+
       return { message: 'Email already registered. New verification code sent.', statusCode: 201 };
     }
-  
+
     // Пользователь не существует или уже верифицирован, создаем нового пользователя
+
     const verificationCode = generateVerificationCode();
     const hashedPassword = await bcrypt.hash(password, 10);
-  
+
     try {
       const user = await this.userModel.create({
         name,
@@ -56,100 +56,21 @@ export class AuthService {
         verificationCode,
         isVerified: false,
       });
-  
-      // Отправляем код подтверждения на электронную почту
+
+// Отправляем код подтверждения на электронную почту
       await this.sendVerificationEmail(user.email, verificationCode);
-  
+
       const token = this.jwtService.sign({ id: user._id });
-    
-      return { message: 'Registration completed successful', statusCode: 201};
+
+      return { message: 'Registration completed successfully', statusCode: 201, token };
     } catch (error) {
       console.error('Error during sign-up:', error);
       return { message: 'Error: Registration failed', statusCode: 500 };
     }
   }  
 
-  async verifyAccount(email: string, verificationCode: string): Promise<{ message: string, statusCode: number }> {
-    const user = await this.userModel.findOne({ email, verificationCode }).exec();
-
-    if (!user) {
-      throw new NotFoundException('Invalid verification code');
-    }
-
-    // Помечаем аккаунт как активированный
-    user.isVerified = true;
-    await user.save();
-    
-      return { message: 'Email validate successful', statusCode: 201 }; // 201 - Created
-    } catch (error) {
-      console.error('Error during sign-up:', error);
-      return { message: 'Error: Email not validated', statusCode: 500 }; // 500 - Internal Server Error
-  }
-
-  async resendVerificationCode(email: string): Promise<{ message: string, statusCode: number }> {
-    const user = await this.userModel.findOne({ email });
-  
-    if (!user) {
-      throw new NotFoundException('User not found');
-    }
-  
-    if (user.isVerified) {
-      return { message: 'User is already verified.', statusCode: 400 };
-    }
-  
-    // Генерируем новый код и сохраняем его в базу данных
-    const newVerificationCode = generateVerificationCode();
-    user.verificationCode = newVerificationCode;
-    await user.save();
-  
-    // Отправляем новый код подтверждения
-    await this.sendVerificationEmail(user.email, newVerificationCode);
-  
-    return { message: 'New verification code sent.', statusCode: 200 };
-  }  
-
-  private async sendVerificationEmail(email: string, verificationCode: string): Promise<void> {
-    // Создаем транспорт для отправки электронных писем (замените значения на свои)
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: parseInt(process.env.SMTP_PORT, 10),
-      secure: true, // true для порта 465, false для других портов
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-      }
-    });
-
-    transporter.verify(function (error) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Server is ready to take our messages");
-      }
-    });
-
-    // Опции письма
-    const mailOptions = {
-      from: process.env.SMTP_USER,
-      to: email,
-      subject: 'Email Verification',
-      text: `Your verification code is: ${verificationCode}`,
-    };  
-
-    // Отправляем письмо
-    await transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error('Error:', error);
-        throw new Error('Failed to send verification email.');
-      } else {
-        console.log('Email sent:', info.response);
-      }
-    });
-  }
-
   async login(loginDto: LoginDto): Promise<{ token: string, statusCode: number }> {
     const { email, password } = loginDto;
-
     const user = await this.userModel.findOne({ email });
 
     if (!user) {
@@ -169,6 +90,83 @@ export class AuthService {
     const token = this.jwtService.sign({ id: user._id });
 
     return { token, statusCode: 201 };
+  }
+
+  async verifyAccount(email: string, verificationCode: string): Promise<{ message: string, statusCode: number }> {
+    const user = await this.userModel.findOne({ email, verificationCode }).exec();
+
+    if (!user) {
+      throw new NotFoundException('Invalid verification code');
+    }
+
+// Помечаем аккаунт как активированный
+    user.isVerified = true;
+    await user.save();
+    
+    return { message: 'Email validate successful', statusCode: 201 }; // 201 - Created
+  }catch (error) {
+      console.error('Error during sign-up:', error);
+      return { message: 'Error: Email not validated', statusCode: 500 }; // 500 - Internal Server Error
+  }
+
+  async resendVerificationCode(email: string): Promise<{ message: string, statusCode: number }> {
+    const user = await this.userModel.findOne({ email });
+  
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+  
+    if (user.isVerified) {
+      return { message: 'User is already verified.', statusCode: 400 };
+    }
+
+   // Генерируем новый код и сохраняем его в базу данных
+    const newVerificationCode = generateVerificationCode();
+    user.verificationCode = newVerificationCode;
+    await user.save();
+
+  // Отправляем новый код подтверждения
+    await this.sendVerificationEmail(user.email, newVerificationCode);
+  
+    return { message: 'New verification code sent.', statusCode: 200 };
+  }
+
+  private async sendVerificationEmail(email: string, verificationCode: string): Promise<void> {
+     // Создаем транспорт для отправки электронных писем (замените значения на свои)
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: parseInt(process.env.SMTP_PORT, 10),
+      secure: true, // true для порта 465, false для других портов
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+      }
+    });
+
+    transporter.verify(function (error) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Server is ready to take our messages");
+      }
+    });
+
+ // Опции письма
+    const mailOptions = {
+      from: process.env.SMTP_USER,
+      to: email,
+      subject: 'Email Verification',
+      text: `Your verification code is: ${verificationCode}`,
+    };
+// Отправляем письмо
+    await transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error:', error);
+        throw new Error('Failed to send verification email.');
+      } else {
+        console.log('Email sent:', info.response);
+      }
+    });
   }
 }
 
