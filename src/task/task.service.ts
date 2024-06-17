eb3b14736ffd5fec3d68d921df6d807c6e5d3cdf
vrungel <arsenyan.m.i20@gmail.com>
2024-06-18 01:07:49 +0300

    creating and working with tasks

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -6,33 +6,28 @@ import { User } from '../user/user.schema';
 import { Project } from '../project/project.schema';
 import { CreateNoteDto } from '../note/dto/create-note.dto';
 import { UpdateNoteDto } from '../note/dto/update-note.dto';
-import { Note } from '../note/note.schema';
+import { Note } from 'src/note/note.schema';
 
 @Injectable()
 export class TaskService {
   private defaultStatuses = ['backlog', 'done', 'in progress', 'todo'];
-
   constructor(
     @InjectModel(Task.name) private taskModel: Model<Task>,
     @InjectModel(User.name) private userModel: Model<User>,
     @InjectModel(Project.name) private projectModel: Model<Project>,
   ) {}
-
   private async validateStatus(projectId: string, status: string): Promise<void> {
     if (this.defaultStatuses.includes(status)) {
       return;
     }
-
     const project = await this.projectModel.findById(projectId);
     if (!project) {
       throw new NotFoundException('Project not found');
     }
-
     if (!project.customStatuses || !project.customStatuses.includes(status)) {
       throw new BadRequestException(`Status '${status}' is not valid for project '${projectId}'`);
     }
   }
-
   async createTask(taskData: {
     title: string;
     description?: string;
@@ -57,7 +52,6 @@ export class TaskService {
       isPersonal,
       viewType = 'backlog',
     } = taskData;
-
     let assignee: User | null = null;
     if (assigneeEmail) {
       assignee = await this.userModel.findOne({ email: assigneeEmail });
@@ -65,7 +59,6 @@ export class TaskService {
         throw new NotFoundException('Assignee not found');
       }
     }
-
     if (projectId) {
       await this.validateStatus(projectId, status);
       const project = await this.projectModel.findById(projectId);
@@ -76,7 +69,6 @@ export class TaskService {
         throw new NotFoundException('Assignee not in the project');
       }
     }
-
     const task = new this.taskModel({
       title,
       description,
@@ -84,15 +76,13 @@ export class TaskService {
       subtasks,
       status,
       notes,
-      assignee: assignee ? assignee.email : undefined,
+      assignee: assignee ? assignee._id : undefined,
       project: projectId ? new Types.ObjectId(projectId) : undefined,
       isPersonal: isPersonal || false,
       viewType,
     });
-
     return task.save();
   }
-
   async updateTask(taskId: string, updateData: {
     title?: string;
     description?: string;
@@ -106,13 +96,12 @@ export class TaskService {
     if (!task) {
       throw new NotFoundException('Task not found');
     }
-
     if (updateData.status) {
       await this.validateStatus(task.project.toString(), updateData.status);
     }
 
     if (updateData.assigneeEmail) {
-      let assignee = await this.userModel.findOne({ email: updateData.assigneeEmail });
+      const assignee = await this.userModel.findOne({ email: updateData.assigneeEmail });
       if (!assignee) {
         throw new NotFoundException('Assignee not found');
       }
@@ -122,26 +111,23 @@ export class TaskService {
           throw new NotFoundException('Assignee not in the project');
         }
       }
-      assignee = User._id;
+      task.assignee = assignee._id;
     }
 
     Object.assign(task, updateData);
-    const updatedTask = await task.save();
-    return this.populateTask(updatedTask);
+    return task.save();
   }
 
   async deleteTask(taskId: string): Promise<void> {
     await this.taskModel.findByIdAndDelete(taskId);
   }
 
-  async getProjectTasks(projectId: string, viewType: string): Promise<any[]> {
+  async getProjectTasks(projectId: string, viewType: string): Promise<Task[]> {
     const project = await this.projectModel.findById(projectId);
     if (!project) {
       throw new NotFoundException('Project not found');
     }
-
     let statusFilter: any;
-
     if (viewType === 'kanban') {
       const kanbanStatuses = [...this.defaultStatuses, ...(project.customStatuses || [])].filter(status => status !== 'backlog');
       statusFilter = { $in: kanbanStatuses };
@@ -149,13 +135,11 @@ export class TaskService {
       statusFilter = 'backlog';
     }
 
-    const tasks = await this.taskModel.find({ project: new Types.ObjectId(projectId), status: statusFilter }).populate('assignee', 'email');
-    return tasks.map(task => this.populateTask(task));
+    return this.taskModel.find({ project: new Types.ObjectId(projectId), status: statusFilter });
   }
 
-  async getUserPersonalTasks(userId: string): Promise<any[]> {
-    const tasks = await this.taskModel.find({ assignee: new Types.ObjectId(userId), isPersonal: true }).populate('assignee', 'email');
-    return tasks.map(task => this.populateTask(task));
+  async getUserPersonalTasks(userId: string): Promise<Task[]> {
+    return this.taskModel.find({ assignee: new Types.ObjectId(userId), isPersonal: true });
   }
 
   async createCustomStatus(projectId: string, status: string): Promise<void> {
@@ -163,40 +147,32 @@ export class TaskService {
     if (!project) {
       throw new NotFoundException('Project not found');
     }
-
     if (!project.customStatuses) {
       project.customStatuses = [];
     }
-
     project.customStatuses.push(status);
     await project.save();
   }
-
   async updateCustomStatus(projectId: string, oldStatus: string, newStatus: string): Promise<void> {
     const project = await this.projectModel.findById(projectId);
     if (!project) {
       throw new NotFoundException('Project not found');
     }
-
     const statusIndex = project.customStatuses.indexOf(oldStatus);
     if (statusIndex === -1) {
       throw new NotFoundException('Status not found');
     }
-
     project.customStatuses[statusIndex] = newStatus;
     await project.save();
   }
-
   async deleteCustomStatus(projectId: string, status: string): Promise<void> {
     const project = await this.projectModel.findById(projectId);
     if (!project) {
       throw new NotFoundException('Project not found');
     }
-
     project.customStatuses = project.customStatuses.filter(s => s !== status);
     await project.save();
   }
-
   async getCustomStatuses(projectId: string): Promise<string[]> {
     const project = await this.projectModel.findById(projectId);
     if (!project) {
@@ -205,28 +181,27 @@ export class TaskService {
     return project.customStatuses;
   }
 
-  async addNoteToTask(taskId: string, createNoteDto: CreateNoteDto): Promise<any> {
+ // Создание заметки в задаче
+ async addNoteToTask(taskId: string, createNoteDto: CreateNoteDto): Promise<Task> {
     const task = await this.taskModel.findById(taskId);
     if (!task) {
       throw new NotFoundException('Task not found');
     }
 
     task.notes.push(createNoteDto as Note);
-    const savedTask = await task.save();
-    return this.populateTask(savedTask);
+  return task.save();
   }
 
-  async updateNoteInTask(taskId: string, noteId: string, updateNoteDto: UpdateNoteDto): Promise<any> {
+// Обновление заметки в задаче
+async updateNoteInTask(taskId: string, noteId: string, updateNoteDto: UpdateNoteDto): Promise<Task> {
     const task = await this.taskModel.findById(taskId);
     if (!task) {
       throw new NotFoundException('Task not found');
     }
-
     const note = task.notes.id(noteId);
     if (!note) {
       throw new NotFoundException('Note not found');
     }
-
     if (updateNoteDto.title) {
       note.title = updateNoteDto.title;
     }
@@ -247,33 +222,25 @@ export class TaskService {
             todo.isCompleted = updatedTodo.isCompleted;
           }
         } else {
-          note.todos.push(updatedTodo as any); // Convert DTO to Todo type
+        note.todos.push(updatedTodo as any); // Преобразуем DTO к типу Todo
         }
       });
     }
 
-    const savedTask = await task.save();
-    return this.populateTask(savedTask);
+  return task.save();
   }
 
-  async deleteNoteFromTask(taskId: string, noteId: string): Promise<any> {
+async deleteNoteFromTask(taskId: string, noteId: string): Promise<Task> {
     const task = await this.taskModel.findById(taskId);
     if (!task) {
       throw new NotFoundException('Task not found');
     }
-
     const note = task.notes.id(noteId);
     if (!note) {
       throw new NotFoundException('Note not found');
     }
 
     note.remove();
-    const savedTask = await task.save();
-    return this.populateTask(savedTask);
-  }
-
-  private async populateTask(task: any): Promise<any> {
-    await task.populate('assignee', 'email').execPopulate();
-    return task;
+  return task.save();
   }
 }
